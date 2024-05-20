2e7a85c98d344a22c2d351b7495c6805c5bd9767
Thiago Barros <bunbismuth@proton.me>
2024-05-19 21:21:29 -0300

    test: added RespostaDTO tests

---
+++ back-spring/src/test/java/com/harmonify/backspring/dominio/dto/RespostaDTOTest.java
@@ -0,0 +1,41 @@
+package com.harmonify.backspring.dominio.dto;
+
+import com.harmonify.backspring.dominio.Musica;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RespostaDTOTest {
+
+  @Test
+  void testConstrutorComMusica() {
+    byte[] foto = {0x12, 0x34, 0x56, 0x78};
+    MusicaDTO musicaDTO = new MusicaDTO("Nome da Música", "Artista", "Rock", "03:45", Date.valueOf("2022-01-01"), foto);
+    Musica musica = new Musica(musicaDTO);
+
+    RespostaDTO respostaDTO = new RespostaDTO(musica);
+
+    assertThat(respostaDTO.nome()).isEqualTo("Nome da Música");
+    assertThat(respostaDTO.artista()).isEqualTo("Artista");
+    assertThat(respostaDTO.generoMusical()).isEqualTo("Rock");
+    assertThat(respostaDTO.duracao()).isEqualTo("03:45");
+    assertThat(respostaDTO.dataLancamento()).isEqualTo(Date.valueOf("2022-01-01"));
+    assertThat(respostaDTO.foto()).isEqualTo(Base64.encodeBase64String(foto));
+  }
+
+  @Test
+  void testConstrutorComValores() {
+    byte[] foto = {0x12, 0x34, 0x56, 0x78};
+    RespostaDTO respostaDTO = new RespostaDTO("Nome da Música", "Artista", "Rock", "03:45", Date.valueOf("2022-01-01"), Base64.encodeBase64String(foto));
+
+    assertThat(respostaDTO.nome()).isEqualTo("Nome da Música");
+    assertThat(respostaDTO.artista()).isEqualTo("Artista");
+    assertThat(respostaDTO.generoMusical()).isEqualTo("Rock");
+    assertThat(respostaDTO.duracao()).isEqualTo("03:45");
+    assertThat(respostaDTO.dataLancamento()).isEqualTo(Date.valueOf("2022-01-01"));
+    assertThat(respostaDTO.foto()).isEqualTo(Base64.encodeBase64String(foto));
+  }
+}
