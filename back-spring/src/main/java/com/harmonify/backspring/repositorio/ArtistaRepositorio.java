9045d7fa40c1ca1d0a40a5494d080b5e1575634f
Guiioff <guilhermerivaldy288@hotmail.com>
2024-05-26 17:05:30 -0300

    feat: criação do método para retornar artistas por gênero no repository

--- back-spring/src/main/java/com/harmonify/backspring/repositorio/ArtistaRepositorio.java
+++ back-spring/src/main/java/com/harmonify/backspring/repositorio/ArtistaRepositorio.java
@@ -3,5 +3,8 @@ package com.harmonify.backspring.repositorio;
 import com.harmonify.backspring.dominio.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArtistaRepositorio extends JpaRepository<Artista, Long> {
+    List<Artista> findByGenero(String genero);
 }
