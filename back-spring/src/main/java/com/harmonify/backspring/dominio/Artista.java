2ea9d73100f2279a472d2a9197d3e1c3cbdb28eb
Guiioff <guilhermerivaldy288@hotmail.com>
2024-05-26 16:51:55 -0300

    feat: adição da anotação @Setter

--- back-spring/src/main/java/com/harmonify/backspring/dominio/Artista.java
+++ back-spring/src/main/java/com/harmonify/backspring/dominio/Artista.java
@@ -5,11 +5,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Table(name = "Artista")
 public class Artista {
 
