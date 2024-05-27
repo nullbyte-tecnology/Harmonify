ecd3644e40a82f7b9af00422e5291a5f30fa87d8
Thiago Barros <bunbismuth@proton.me>
2024-05-26 21:17:36 -0300

    refactor: mudanças na arquitetura do projeto e padronização de nomenclaturas

---
+++ back-spring/src/main/java/com/harmonify/backspring/api/contracts/requests/ArtistaDTO.java
@@ -0,0 +1,12 @@
+package com.harmonify.backspring.api.contracts.requests;
+
+import com.harmonify.backspring.domain.models.Artista;
+
+public record ArtistaDTO(String nome, String genero, String nacionalidade, String biografia,
+                         String paisOrigem) {
+
+  public ArtistaDTO(Artista artista) {
+    this(artista.getNome(), artista.getGenero(), artista.getNacionalidade(),
+        artista.getNacionalidade(), artista.getPaisOrigem());
+  }
+}
---
+++ back-spring/src/main/java/com/harmonify/backspring/api/controllers/ArtistaControlador.java
@@ -0,0 +1,45 @@
+package com.harmonify.backspring.api.controllers;
+
+import com.harmonify.backspring.api.contracts.requests.ArtistaDTO;
+import com.harmonify.backspring.domain.services.ArtistaServico;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/artistas")
+public class ArtistaControlador {
+
+  private final ArtistaServico artistaServico;
+
+  public ArtistaControlador(ArtistaServico artistaServico) {
+    this.artistaServico = artistaServico;
+  }
+
+  @GetMapping
+  public List<ArtistaDTO> listarArtistas() {
+    return artistaServico.listarArtistas();
+  }
+
+  @PostMapping
+  public void salvarArtista(ArtistaDTO artistaDTO) {
+    artistaServico.salvarArtista(artistaDTO);
+  }
+
+  @PutMapping("/{id}")
+  public void atualizarArtista(@PathVariable Long id, @RequestBody ArtistaDTO artistaDTO) {
+    artistaServico.atualizarArtista(id, artistaDTO);
+  }
+
+  @GetMapping("/genero/{genero}")
+  public List<ArtistaDTO> listarArtistasPorGenero(@PathVariable String genero) {
+    return artistaServico.listarArtistasPorGenero(genero);
+  }
+
+
+}
