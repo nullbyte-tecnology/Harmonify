5e788c6c03f2de7962115e3384e40dfe6502a9f0
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 11:32:44 -0300

    feat: implementação do bean de configuração da cadeia de filtros de segurança

--- back-spring/src/main/java/com/harmonify/backspring/infrastructure/security/ConfiguracaoSeguranca.java
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/security/ConfiguracaoSeguranca.java
@@ -3,13 +3,19 @@ package com.harmonify.backspring.infrastructure.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,5 +37,21 @@ public class ConfiguracaoSeguranca {
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
       throws Exception {
     return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            request -> {
+              request.requestMatchers(HttpMethod.POST, "/api/auth/cadastro").permitAll();
+              request.requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll();
+              request.anyRequest().authenticated();
+            })
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(this.filtroSeguranca, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
   }
 }
