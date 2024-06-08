643f621decd0635b5ea2823cf2b3a3ebf0d0f66e
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 11:28:41 -0300

    feat: implementação dos beans para retorno de provedor e gerenciador de autenticação

--- back-spring/src/main/java/com/harmonify/backspring/infrastructure/security/ConfiguracaoSeguranca.java
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/security/ConfiguracaoSeguranca.java
@@ -1,15 +1,35 @@
 package com.harmonify.backspring.infrastructure.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class ConfiguracaoSeguranca {
   private final FiltroSeguranca filtroSeguranca;
-  private final SenhaEncoderBean senhaEncoderBean;
+  private final PasswordEncoder passwordEncoder;
   private final UserDetailsService userDetailsService;
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(this.userDetailsService);
+    provider.setPasswordEncoder(this.passwordEncoder);
+    return provider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
 }
