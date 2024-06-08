1afe5864a8ea00f52776e02dec3b492f5f6f2da6
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 10:39:30 -0300

    feat: configuração da classe Usuario com as anotações da JPA e Lombok

--- back-spring/src/main/java/com/harmonify/backspring/domain/models/Usuario.java
+++ back-spring/src/main/java/com/harmonify/backspring/domain/models/Usuario.java
@@ -1,9 +1,24 @@
 package com.harmonify.backspring.domain.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.UUID;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Usuario {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(nullable = false, unique = true, length = 75)
   private String login;
+
+  @Column(nullable = false, length = 50)
   private String senha;
 }
