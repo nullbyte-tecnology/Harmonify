a2bcdb3aeb5772e74e7e26e389c15bba9df62480
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 11:20:02 -0300

    refactor: alteração do nome da role de usuário

--- back-spring/src/main/java/com/harmonify/backspring/domain/models/Usuario.java
+++ back-spring/src/main/java/com/harmonify/backspring/domain/models/Usuario.java
@@ -29,7 +29,7 @@ public class Usuario implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority("USER"));
+    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
   }
 
   @Override
