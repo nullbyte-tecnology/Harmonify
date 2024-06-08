592331da66cac750b5f6fb93f90bcc336d449bd1
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 11:02:09 -0300

    feat: implementação do método de geração do token de autenticação

--- back-spring/src/main/java/com/harmonify/backspring/domain/services/TokenService.java
+++ back-spring/src/main/java/com/harmonify/backspring/domain/services/TokenService.java
@@ -1,7 +1,15 @@
 package com.harmonify.backspring.domain.services;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -16,5 +24,23 @@ public class TokenService {
 
     this.secretKey = secretKey;
     this.tokenIssuer = tokenIssuer;
+  }
+
+  public String gerarToken(UserDetails usuario) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
+
+      return JWT.create()
+          .withIssuer(this.tokenIssuer)
+          .withSubject(usuario.getUsername())
+          .withExpiresAt(gerarExpiracao())
+          .sign(algorithm);
+    } catch (JWTCreationException ex) {
+      throw new RuntimeException("Erro ao gerar token de autenticação");
+    }
+  }
+
+  private Instant gerarExpiracao() {
+    return LocalDateTime.now().plusHours(DURACAO_TOKEN_HORAS).toInstant(ZoneOffset.of("-03:00"));
   }
 }
