6610f8e0978943b6d27db016eb18149beba5cb21
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 11:15:05 -0300

    feat: criação do filtro de segurança para a autenticação

---
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/security/FiltroSeguranca.java
@@ -0,0 +1,28 @@
+package com.harmonify.backspring.infrastructure.security;
+
+import com.harmonify.backspring.domain.services.TokenServico;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class FiltroSeguranca extends OncePerRequestFilter {
+  private final TokenServico tokenServico;
+  private final UserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain)
+      throws ServletException, IOException {}
+}
