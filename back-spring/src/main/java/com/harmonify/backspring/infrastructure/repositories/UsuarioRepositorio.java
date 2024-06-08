7355c2e63daf0ef3f279246605560acfad68388f
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 11:11:34 -0300

    feat: declaração de query method para buscar usuário pelo login

--- back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/UsuarioRepositorio.java
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/UsuarioRepositorio.java
@@ -3,6 +3,9 @@ package com.harmonify.backspring.infrastructure.repositories;
 import com.harmonify.backspring.domain.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UsuarioRepositorio extends JpaRepository<Usuario, UUID> {}
+public interface UsuarioRepositorio extends JpaRepository<Usuario, UUID> {
+  Optional<Usuario> findByLogin(String login);
+}
