b3d260a20d038421bfcfccd0eb9ab19df0e4ca4d
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 10:43:59 -0300

    feat: criação da interface de repositório para a entidade usuário

---
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/UsuarioRepositorio.java
@@ -0,0 +1,8 @@
+package com.harmonify.backspring.infrastructure.repositories;
+
+import com.harmonify.backspring.domain.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario, UUID> {}
