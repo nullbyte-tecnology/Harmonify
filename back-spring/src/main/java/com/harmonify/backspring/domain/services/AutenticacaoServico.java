8eb3009dda00d3571fc6ffc44d677fb4b6bca83f
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-08 23:12:46 -0300

    feat: implementação do método de realização do login

--- back-spring/src/main/java/com/harmonify/backspring/domain/services/AutenticacaoServico.java
+++ back-spring/src/main/java/com/harmonify/backspring/domain/services/AutenticacaoServico.java
@@ -1,10 +1,15 @@
 package com.harmonify.backspring.domain.services;
 
+import com.harmonify.backspring.api.contracts.requests.LoginDTO;
 import com.harmonify.backspring.api.contracts.requests.UsuarioDTO;
 import com.harmonify.backspring.api.contracts.responses.TokenDTO;
 import com.harmonify.backspring.domain.models.Usuario;
 import com.harmonify.backspring.infrastructure.repositories.UsuarioRepositorio;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +19,8 @@ public class AutenticacaoServico {
   private final PasswordEncoder passwordEncoder;
   private final UsuarioRepositorio usuarioRepositorio;
   private final TokenServico tokenServico;
+  private final AuthenticationManager authenticationManager;
+  private final UserDetailsService userDetailsService;
 
   public TokenDTO registrarUsuario(UsuarioDTO usuarioDTO) {
     if (this.usuarioRepositorio.existsByLogin(usuarioDTO.login())) {
@@ -25,6 +32,16 @@ public class AutenticacaoServico {
     Usuario usuarioSalvo = this.usuarioRepositorio.save(novoUsuario);
 
     String token = this.tokenServico.gerarToken(usuarioSalvo);
+
+    return new TokenDTO(token);
+  }
+
+  public TokenDTO realizarLogin(LoginDTO loginDTO) {
+    this.authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(loginDTO.login(), loginDTO.senha()));
+
+    UserDetails usuario = this.userDetailsService.loadUserByUsername(loginDTO.login());
+    String token = this.tokenServico.gerarToken(usuario);
 
     return new TokenDTO(token);
   }
