0faeb219a03df63ac75d6e492ea30793d76f9002
Thiago Barros <bunbismuth@proton.me>
2024-06-13 19:49:03 -0300

    refactor: ArtistaDTO refatorado

--- back-spring/src/main/java/com/harmonify/backspring/api/contracts/requests/ArtistaDTO.java
+++ back-spring/src/main/java/com/harmonify/backspring/api/contracts/requests/ArtistaDTO.java
@@ -3,11 +3,11 @@ package com.harmonify.backspring.api.contracts.requests;
 import com.harmonify.backspring.domain.models.Artista;
 import com.harmonify.backspring.domain.models.enums.GeneroMusical;
 
-public record ArtistaDTO(String nome, GeneroMusical genero, byte[] foto, String biografia,
-                         String paisOrigem) {
+public record ArtistaDTO(String nome, byte[] foto, String biografia,
+                         String paisOrigem, GeneroMusical genero) {
 
   public ArtistaDTO(Artista artista) {
-    this(artista.getNome(), artista.getGenero(), artista.getFoto(),
-        artista.getBiografia(), artista.getPaisOrigem());
+    this(artista.getNome(), artista.getFoto(),
+        artista.getBiografia(), artista.getPaisOrigem(), artista.getGenero());
   }
 }
