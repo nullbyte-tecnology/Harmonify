41a8f788c1a3fba768495c2461634d62584b1665
Guiioff <guilhermerivaldy288@hotmail.com>
2024-06-14 20:46:35 -0300

    refactor: alterando nome do UUID do artista no DTO

--- back-spring/src/main/java/com/harmonify/backspring/api/contracts/requests/MusicaAlbumDTO.java
+++ back-spring/src/main/java/com/harmonify/backspring/api/contracts/requests/MusicaAlbumDTO.java
@@ -2,5 +2,5 @@ package com.harmonify.backspring.api.contracts.requests;
 
 import java.util.UUID;
 
-public record MusicaAlbumDTO(String nome, UUID artista) {
+public record MusicaAlbumDTO(String nome, UUID idArtista) {
 }
