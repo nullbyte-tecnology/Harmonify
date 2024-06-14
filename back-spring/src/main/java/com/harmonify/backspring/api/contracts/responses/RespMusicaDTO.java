ac8846fbf49e8b7cef3d88d9dfa3544afd8cb4ee
Thiago Barros <bunbismuth@proton.me>
2024-06-13 21:07:57 -0300

    refactor: RespostaDTO refatorado para RespMusicaDTO

--- back-spring/src/main/java/com/harmonify/backspring/api/contracts/responses/RespMusicaDTO.java
+++ back-spring/src/main/java/com/harmonify/backspring/api/contracts/responses/RespMusicaDTO.java
@@ -4,10 +4,9 @@ import com.harmonify.backspring.domain.models.Musica;
 import java.sql.Date;
 import org.apache.tomcat.util.codec.binary.Base64;
 
-public record RespostaDTO(String nome, String artista, String generoMusical, String duracao,
-                          Date dataLancamento, String foto) {
-
-  public RespostaDTO(Musica musica) {
+public record RespMusicaDTO(String nome, String artista, String generoMusical, String duracao,
+                            Date dataLancamento, String foto) {
+  public RespMusicaDTO(Musica musica) {
     this(musica.getNome(), musica.getArtista().getNome(), musica.getGenero(), musica.getDuracaoSegundos(),
         musica.getLancamento(), Base64.encodeBase64String(musica.getFoto()));
   }
