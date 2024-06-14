1c52e03e864eafa3fbb17448c1c05b312314de8c
Guiioff <guilhermerivaldy288@hotmail.com>
2024-06-14 20:14:28 -0300

    feat: criação do método para procurar uma música de acordo com o nome e id do artista

--- back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/MusicaRepositorio.java
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/MusicaRepositorio.java
@@ -3,6 +3,10 @@ package com.harmonify.backspring.infrastructure.repositories;
 import com.harmonify.backspring.domain.models.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface MusicaRepositorio extends JpaRepository<Musica, Long> {
 
+    Optional<Musica> findByNomeAndArtistaId(String nome, UUID idArtista);
 }
