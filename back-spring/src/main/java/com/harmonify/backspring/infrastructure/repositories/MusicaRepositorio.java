8ba6dbf107c1d42abced6ee857bad5363a176ddd
Guiioff <guilhermerivaldy288@hotmail.com>
2024-06-14 23:11:53 -0300

    feat: resolvendo conflito

--- back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/MusicaRepositorio.java
+++ back-spring/src/main/java/com/harmonify/backspring/infrastructure/repositories/MusicaRepositorio.java
@@ -1,12 +1,17 @@
 package com.harmonify.backspring.infrastructure.repositories;
 
 import com.harmonify.backspring.domain.models.Musica;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import jakarta.annotation.Nullable;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MusicaRepositorio extends JpaRepository<Musica, Long> {
+public interface MusicaRepositorio extends JpaRepository<Musica, UUID>,
+        JpaSpecificationExecutor<Musica> {
 
+    List<Musica> findAll(@Nullable Specification<Musica> spec);
     Optional<Musica> findByNomeAndArtistaId(String nome, UUID idArtista);
 }
