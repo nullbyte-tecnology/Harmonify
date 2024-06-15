63d0eccfc516f09a1f499379e37b9caed7dccc48
Guiioff <guilhermerivaldy288@hotmail.com>
2024-06-14 21:23:18 -0300

    feat: criação do método para detalhar um album no controller

--- back-spring/src/main/java/com/harmonify/backspring/api/controllers/AlbumControlador.java
+++ back-spring/src/main/java/com/harmonify/backspring/api/controllers/AlbumControlador.java
@@ -1,10 +1,13 @@
 package com.harmonify.backspring.api.controllers;
 
 import com.harmonify.backspring.api.contracts.requests.AlbumDTO;
+import com.harmonify.backspring.api.contracts.responses.RespAlbumDTO;
 import com.harmonify.backspring.domain.services.AlbumServico;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/albuns")
@@ -14,8 +17,14 @@ public class AlbumControlador {
     private final AlbumServico albumServico;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.CREATED)
     public void salvarAlbum(@RequestBody AlbumDTO albumDTO){
         albumServico.salvarAlbum(albumDTO);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RespAlbumDTO detalharAlbum(@PathVariable UUID id){
+        return albumServico.detalharAlbum(id);
     }
 }
